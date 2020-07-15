aa04120aa381d12e555bf2052ef73eab7363abe9
zym <zym_zom@mail.ru>
2020-07-15 20:09:00 +0300

    add generics and mixins

--- books/api/v1/urls.py
+++ books/api/v1/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 
 from books.api.v1.views import (
     BooksView,
-    FindBook)
+    FindBook,
+    BooksListView)
 
 app_name = 'books'
 urlpatterns = [
-    path('', BooksView.as_view()),
-
-    path('<id>', FindBook.as_view()),
-
+    # path('', BooksView.as_view()),
+    # path('<id>', FindBook.as_view()),
+    path('', BooksListView.as_view()),
+    path('<int:id>', BooksListView.as_view()),
 ]
--- books/api/v1/views.py
+++ books/api/v1/views.py
@@ -1,37 +1,51 @@
-
 from rest_framework import status
 
 from rest_framework.response import Response
-
+from rest_framework import generics
+from rest_framework import mixins
 from rest_framework.views import APIView
 
 from books.models import Book
 from books.api.v1.serializers import BookSerializer
 
 
-class BooksView(APIView):
-    def get(self, request):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
+class BooksListView(generics.GenericAPIView,
+                    mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    lookup_field = 'id'
 
-    def post(self,request):
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request, id)
+        else:
+            return self.list(request)
 
+    def post(self, request):
+        return self.create(request)
 
-class FindBook(APIView):
-    def get(self, request, id):
-        try:
-            book = Book.objects.get(id=id)
-        except Book.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = BookSerializer(book)
-        return Response(serializer.data)
-
-
-
+# class BooksView(APIView):
+#     def get(self, request):
+#         books = Book.objects.all()
+#         serializer = BookSerializer(books, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         serializer = BookSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class FindBook(APIView):
+#     def get(self, request, id):
+#         try:
+#             book = Book.objects.get(id=id)
+#         except Book.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#         serializer = BookSerializer(book)
+#         return Response(serializer.data)
