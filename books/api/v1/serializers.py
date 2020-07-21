9485f0c89ac04ba34918a63a2a5892ab9fcad5fa
Your Namezym43210 <zymzom@yandex.ru>
2020-07-21 09:24:48 +0300

    crud author

--- books/api/v1/serializers.py
+++ books/api/v1/serializers.py
@@ -5,14 +5,13 @@ from books.models import Book
 from authors.models import Author
 
 
-class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True, max_length=50)
-    author_name = serializers.CharField(required=True, max_length=50)
-    description = serializers.CharField(required=True, max_length=500)
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['title', 'description', 'author_name']
 
     def create(self, validated_data):
         name = validated_data['author_name']
         validated_data['author_name'] = Author.objects.get(name=name)
-
+        print(validated_data['author_name'])
         return Book.objects.create(**validated_data)
